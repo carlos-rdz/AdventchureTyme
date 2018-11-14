c5d959dea5d0c07adbaf7ee97f4711db203077e0
Carlos Rodriguez <carlos.a.rodriguez100@gmail.com>
2018-11-14 15:50:31 -0500

    adds user class and intro to cloudinary

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const users = require('./models/users');
+
+const cloudinary = require('cloudinary');
+cloudinary.config({ 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.CLOUD_KEY, 
+    api_secret: process.env.CLOUD_SECRET
+  });
+  
+
+  let test = cloudinary.v2.uploader.upload("./images/stonemtn.jpeg", {phash:true},
+  function(error, result) {console.log(result, error)})
+    .then(console.log(test.phash));
+
+
---
+++ models/users.js
@@ -0,0 +1,64 @@
+const db = require('./db');
+
+
+class User {
+    constructor(id,name) {
+        this.id = id;
+        this.name = name;
+    }
+// CREATE
+    static createUser(name){
+        return db.one(`insert into users
+        (name)
+        values
+        ($1)
+        returning id`,[name])
+            .then(data => {
+                return new User (data.id, name);
+            })
+}
+
+
+
+// RETRIEVE
+
+    static getUserById(id){
+        return db.one(
+        `select * from users where id=$1`,[id]
+        )
+        .then(data => {
+            return new User (data.id,data.name)
+        })
+    }
+
+
+
+    static getAllUsers(){
+
+        return db.any(
+            `select * from users`
+        ).then(data => {
+            let userArray = data.map(indUser => {
+                return new User (indUser.id,indUser.name)
+            })
+        return userArray
+        })
+
+
+    }
+
+
+// UPDATE
+
+
+
+
+// DELETE
+
+
+
+
+
+}
+
+module.exports = User
