c2ad7b06de4f324b9cf831768b5acd6a6db705b4
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-11-19 11:38:30 -0500

    adds ability to run google vision for testing capability

--- scripts/visionOCR.js
+++ scripts/visionOCR.js
@@ -1,7 +1,7 @@
 // Vison OCR api
 const vision = require('@google-cloud/vision');
 
- // This shell cmd maps where the authentication json is stored. Will the json file need to go on the server then?
+// This shell cmd maps where the authentication json is stored. Will the json file need to go on the server then?
 // export GOOGLE_APPLICATION_CREDENTIALS="/Users/mblatchford/Digital Crafts/DigitalCrafts-ScavengerHunt-b7df5a3616de.json"
 
 
@@ -15,7 +15,7 @@ const client = new vision.ImageAnnotatorClient();
 /**
  * TODO(developer): Uncomment the following line before running the sample.
  */
-//  const imgAddress = './images/IMG_6773.jpg';
+// const imgAddress = './images/IMG_7882.jpg';
 
 // Performs text detection on the local file
 function extractText(imgAddress){
@@ -24,7 +24,7 @@ function extractText(imgAddress){
   .then(results => {
     const detections = results[0].textAnnotations;
     console.log('Text:');
-    // detections.forEach(text => console.log(text));
+    detections.forEach(text => console.log(text));
     return detections;
   })
   .catch(err => {
@@ -58,7 +58,7 @@ function extractText(imgAddress){
 //   });
 // }
 
-
+// extractText(imgAddress);
 
 
 
