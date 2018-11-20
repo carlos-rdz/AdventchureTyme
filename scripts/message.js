d1a3a91c4b605039fdb5707704d5953fceba968e
Michael Blatchford <mblatchford@mac.com>
2018-11-20 16:11:13 -0500

    Moved twilio cred to .env

--- scripts/message.js
+++ scripts/message.js
@@ -1,10 +1,8 @@
 const twilio = require('twilio');
+// const accountSid = 'AC83299797729e54b4fcf99251ee06ed97'; // Your Account SID from www.twilio.com/console
+// const authToken = '6ac6215052f3743a04bd38491cb9669c';   // Your Auth Token from www.twilio.com/console
 
-// need to shift these credentials out of this file
-const accountSid = 'AC83299797729e54b4fcf99251ee06ed97'; // Your Account SID from www.twilio.com/console
-const authToken = '6ac6215052f3743a04bd38491cb9669c';   // Your Auth Token from www.twilio.com/console
-
-let client = new twilio(accountSid, authToken);
+let client = new twilio(process.env.ACCOUNT_SID , process.env.AUTH_TOKEN);
 // let adventureName = "Test";
 // let from = '+16789448410';
 // let to = '+17146093784';
--- scripts/twilioCred.js
+++
@@ -1,3 +0,0 @@
-//  twilio creds
-ACCOUNT_SID = 'AC83299797729e54b4fcf99251ee06ed97'; // Your Account SID from www.twilio.com/console
-AUTH_TOKEN = '6ac6215052f3743a04bd38491cb9669c';   // Your Auth Token from www.twilio.com/console
